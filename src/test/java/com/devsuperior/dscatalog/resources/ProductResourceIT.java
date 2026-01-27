e805732e7335901511ee1a866d94258c09324f74
Heverton <heverton@gmail.com>
2026-01-27 07:33:18 -0300

    Teste de integração na camada web update

--- src/test/java/com/devsuperior/dscatalog/resources/ProductResourceIT.java
+++ src/test/java/com/devsuperior/dscatalog/resources/ProductResourceIT.java
@@ -1,6 +1,10 @@
 package com.devsuperior.dscatalog.resources;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.tests.Factory;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,6 +27,9 @@ public class ProductResourceIT {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     private Long existingId;
     private Long nonexistingId;
     private Long countTotalProducts;
@@ -32,8 +40,9 @@ public class ProductResourceIT {
         nonexistingId = 1000L;
         countTotalProducts = 25L;
     }
+
     @Test
-            public void findAllShouldReturnSortedPageWhenSortByName() throws Exception {
+    public void findAllShouldReturnSortedPageWhenSortByName() throws Exception {
         ResultActions result = mockMvc.perform(
                 get("/products?page=0&size=12&sort=name,asc")
                         .accept(MediaType.APPLICATION_JSON)
@@ -47,4 +56,40 @@ public class ProductResourceIT {
         result.andExpect(jsonPath("$.content[2].name").value("PC Gamer Alfa"));
     }
 
+    @Test
+    public void updateShouldReturnProductDTOWhenIdExists() throws Exception {
+        ProductDTO productDTO = Factory.createProductDTO();
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+        String expectedName = productDTO.getName();
+        String expectedDescription = productDTO.getDescription();
+
+
+        ResultActions result =
+                mockMvc.perform(put("/products/{id}", existingId).content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.id").value(existingId));
+        result.andExpect(jsonPath("$.name").value(expectedName));
+        result.andExpect(jsonPath("$.description").value(expectedDescription));
+
+
+
+    }
+    @Test
+    public void updateShouldReturnNotFoundWhenDoesNotExist() throws Exception {
+        ProductDTO productDTO = Factory.createProductDTO();
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+        String expectedName = productDTO.getName();
+        String expectedDescription = productDTO.getDescription();
+
+
+        ResultActions result =
+                mockMvc.perform(put("/products/{id}", nonexistingId).content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isNotFound());
+}
 }
