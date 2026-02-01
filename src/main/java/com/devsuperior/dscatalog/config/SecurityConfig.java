34e92af7d1a26aaaf88ebcada100cc585e974639
Heverton <heverton@gmail.com>
2026-02-01 16:35:17 -0300

    User CRUD

--- src/main/java/com/devsuperior/dscatalog/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/dscatalog/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscatalog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,9 +12,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf -> csrf.disable()); // Desativa CSRF
-        http.headers(headers -> headers.frameOptions(frame -> frame.disable()));//permite o h2 console
-        return http.build();
+        http
+                // desabilita CSRF (necessário para H2)
+                .csrf(csrf -> csrf.disable())
 
-}
+                // libera acesso a tudo
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+
+                // libera uso de frames (H2 Console usa iframe)
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()));
+
+        return http.build();
+    }
 }
